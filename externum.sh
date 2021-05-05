d7f2c7972b47e83c9aecc1bc8d94ef5ca06757b3
Iain Spinks-Gillen <iain.spinksgillen@hotmail.co.uk>
2021-05-05 20:59:04 +0100

    fied folder and nmap issues and added nmap output formating

--- externum.sh
+++ externum.sh
@@ -6,51 +6,52 @@
 clear
 read -p "Enter the name of the job folder (e.g. client_ext_data) : " mainfolder
 
-if [ ! -d "$makefolder" ]' then'
+if [ ! -d "$mainfolder" ]; then
     mkdir ~/$mainfolder
 fi
 
-cd $mainfolder
-if [ ! -d "nmap" ] then
+cd ~/$mainfolder
+if [ ! -d "nmap" ]; then
     mkdir nmap
 fi
 
-if [ ! -d "enum" ] then
+if [ ! -d "enum" ]; then
     mkdir enum
 fi
 
-if [ ! -d "enum/dns" ] then
+if [ ! -d "enum/dns" ]; then
     mkdir enum/dns
 fi
 
-if [ ! -d "enum/screenshots" ] then
+if [ ! -d "enum/screenshots" ]; then
     mkdir enum/screenshots
 fi
 
-if [ ! -d "nikto" ] then
+if [ ! -d "nikto" ]; then
     mkdir nikto
 fi
 
-if [ ! -d "enum/directories" ] then
+if [ ! -d "enum/directories" ]; then
     mkdir enum/directories
 fi
 
-if [ ! -d "automated" ] then
+if [ ! -d "automated" ]; then
     mkdir automated
 fi
 echo "all job folders created"
 
 # creates a targets file
 cd ~/$mainfolder
-read -p "A targets file will now be created, please paste the in-scope IP's" null
+read -p "A targets file will now be created. Press enter to add IP's to scope" null
 nano targets.txt
 
 # find hostnames associated to IP's
 cd nmap
 echo "Finding hostnames associated to IP's"
-for line in $(cat ../targets.txt); do echo $line" - "$(dig -x $line +short) >> ips_resolved.txt; done
+for line in $(cat ../targets.txt); do echo $line" - "$(dig -x $line +short) >> ../enum/dns/ips_resolved.txt; done
+wait
 
-# run nmap against hosts (need to multi-thread this)
+# run nmap against hosts (need to multi-process this)
 echo "---Starting NMAP TCP/UDP against targets - Assuming all hosts up---"
 
 echo "--TCP--"
@@ -62,11 +63,20 @@ done
 echo "--UDP--"
 for i in $(cat ../targets.txt)
 do
-    nmap -Pn -sSUV $i --tops-ports 200 -oN "$i"_udp 
+    nmap -Pn -sSUV $i --top-ports 200 -oN "$i"_udp 
 done
+wait
+
+# format results from nmap scans
+echo "cleaning nmap results"
+grep -Hari "/tcp" | cut -d "/" -f 1 >> allservices.txt
+grep -Hari "/udp" | cut -d "/" -f 1 >> allservices.txt
+
+#httpprobe services for potential webservers - output webservers.txt
 
 
-#5 - httpprobe services for potential webservers - output webservers.txt
+
+
 #6 - eyewitness against namp output and save to folder
 #7 - directory brute all webservers
 #8 - nikto all webservers
