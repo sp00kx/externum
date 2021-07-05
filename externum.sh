48627c0c917a0dcded7cb7c4ad170b2b9c27447f
Iain Spinks-Gillen <iain.spinksgillen@hotmail.co.uk>
2021-07-05 20:10:44 +0100

    v1.1

--- externum.sh
+++ externum.sh
@@ -6,7 +6,7 @@ ipinfo="https://ipinfo.io"
 # Colours
 red="\e[31m"
 blue="\e[34m"
-green="\e[32;5;48m"
+green="\e[32;48m"
 colouroff="\e[0m"
 
 # Opening functions
@@ -27,7 +27,7 @@ usage () {
     echo "gathering and enumeration."
     echo
     echo "The tool will initially discover hostnames associated to the target IP's"
-    echo "then perform some subdomain enumeration before morning on to trying to"
+    echo "then perform some subdomain enumeration before moving on to trying to"
     echo "identify some quick wins."
     echo
     echo "Once this is complete it should give you enough information to get on with"
@@ -82,36 +82,34 @@ done
 
 
 # Create master folder and subfolders
-echo
-read -r -p 'Enter the name of the job folder (e.g. client_ext_data) : ' mainfolder
-
-if [ ! -d ./"$mainfolder" ]; then
-    mkdir "$mainfolder"
-    mkdir "$mainfolder"/enum
-    mkdir "$mainfolder"/enum/nmap
-    mkdir "$mainfolder"/enum/nmap/tcp
-    mkdir "$mainfolder"/enum/nmap/udp
-    mkdir "$mainfolder"/enum/dns
-    mkdir "$mainfolder"/enum/screenshots
-    mkdir "$mainfolder"/enum/screenshots/captures
-    mkdir "$mainfolder"/enum/nikto
-    mkdir "$mainfolder"/enum/directories
-    touch "$mainfolder"/logfile.txt
+echo "Creating job folders"
+if [ ! -d "./externum-results" ]; then
+    mkdir externum-results
+    mkdir externum-results/enum
+    mkdir externum-results/enum/nmap
+    mkdir externum-results/enum/nmap/tcp
+    mkdir externum-results/enum/nmap/udp
+    mkdir externum-results/enum/dns
+    mkdir externum-results/enum/screenshots
+    mkdir externum-results/enum/screenshots/captures
+    mkdir externum-results/enum/nikto
+    mkdir externum-results/enum/directories
+    touch externum-results/logfile.txt
 fi
 usr_file_path=$( realpath "$target_file" )
-cp "$usr_file_path" "$mainfolder"/
+cp "$usr_file_path" ./externum-results/targets.txt
 sleep 1
 
 osint_enum() {
     # find hostnames associated to IP's
-    echo "Time: $(date -Iseconds). osint started." >> "$mainfolder"/logfile.txt
+    echo "Time: $(date -Iseconds). osint started." >> externum-results/logfile.txt
     echo 
     echo -e "${blue}Hostnames associated to IP's:${colouroff}"
-    for ip in $(cat "$mainfolder"/targets.txt) ; do
+    for ip in $(cat ./externum-results/targets.txt) ; do
         ip_info=$(curl -s $ipinfo/"$ip"/hostname)
-            echo "$ip_info" | cut -d "." -f 2-4 | sort -u >> "$mainfolder"/enum/dns/resolved_tlds.txt;
+            echo "$ip_info" | cut -d "." -f 2-4 | sort -u >> ./externum-results/enum/dns/resolved_tlds.txt;
     done
-    cat "$mainfolder"/enum/dns/resolved_tlds.txt
+    cat ./externum-results/enum/dns/resolved_tlds.txt
 
     # Enumerate subdomains to associated IPs and store for manual analysis
     echo
@@ -123,15 +121,15 @@ osint_enum() {
         echo -e ${red}"Badtimes! You need Amass installed for Externum to work"${colouroff}
         exit
     else
-        for line in $(cat "$mainfolder"/enum/dns/resolved_tlds.txt);
-            do  amass enum -ipv4 -silent -o "$mainfolder"/enum/dns/enumerated_subdomains.txt -d "$line" &
+        for line in $(cat ./externum-results/enum/dns/resolved_tlds.txt);
+            do  amass enum -ipv4 -silent -o ./externum-results/enum/dns/enumerated_subdomains.txt -d "$line" &
             spinner
         done
     fi
     # Add in a wc result for how many subdomains enumerated
     echo
-    echo -e "$blue"Number of subdomains found:"$colouroff"; wc -l < "$mainfolder"/enum/dns/enumerated_subdomains.txt
-    echo "Time: $(date -Iseconds). osint completed." >> "$mainfolder"/logfile.txt
+    echo -e "$blue"Number of subdomains found:"$colouroff"; wc -l < ./externum-results/enum/dns/enumerated_subdomains.txt
+    echo "Time: $(date -Iseconds). osint completed." >> ./externum-results/logfile.txt
 }
 
 
@@ -139,7 +137,7 @@ osint_enum() {
 quickenum() {
     # looking for quick win webservers
     echo
-    echo "Time: $(date -Iseconds). common port enum started." >> "$mainfolder"/logfile.txt
+    echo "Time: $(date -Iseconds). common port enum started." >> ./externum-results/logfile.txt
     echo -e "${blue}Looking for common open services, please be patient${colouroff}"
     # check if Naabu is installed
     if ! command -v naabu &>/dev/null; then
@@ -147,25 +145,25 @@ quickenum() {
         echo -e ${red}"Badtimes! You need Naabo installed for Externum to work"${colouroff}
         exit
     else
-        naabu -p 80,443,8080,8443,8005,8009,8181,4848,9000,8008,9990,7001,9043,9060,9080,9443,1527,7777,4443 -iL "$mainfolder"/targets.txt -silent -o "$mainfolder"/enum/potentialwebservers.txt > /dev/null 2>&1 &
+        naabu -p 80,443,8080,8443,8005,8009,8181,4848,9000,8008,9990,7001,9043,9060,9080,9443,1527,7777,4443 -iL ./externum-results/targets.txt -silent -o ./externum-results/enum/potentialwebservers.txt > /dev/null 2>&1 &
         spinner
     fi
-    echo "Time: $(date -Iseconds). common port enum completed." >> "$mainfolder"/logfile.txt
+    echo "Time: $(date -Iseconds). common port enum completed." >> ./externum-results/logfile.txt
    
     #httprobe
     echo
-    echo "Time: $(date -Iseconds). probing for active webservers." >> "$mainfolder"/logfile.txt
+    echo "Time: $(date -Iseconds). probing for active webservers." >> ./externum-results/logfile.txt
     echo -e "${blue}Probing discovered services for active webservers${colouroff}"
-    cat "$mainfolder"/enum/potentialwebservers.txt | httprobe >> "$mainfolder"/enum/webservers.txt
+    cat ./externum-results/enum/potentialwebservers.txt | httprobe >> ./externum-results/enum/webservers.txt
     echo
-    echo -e "${blue}Number of active webservers found:${colouroff}"; wc -l < "$mainfolder"/enum/webservers.txt
-    echo "Time: $(date -Iseconds). webserver probe complete." >> "$mainfolder"/logfile.txt
+    echo -e "${blue}Number of active webservers found:${colouroff}"; wc -l < ./externum-results/enum/webservers.txt
+    echo "Time: $(date -Iseconds). webserver probe complete." >> ./externum-results/logfile.txt
 
     #directory enum
     echo
-    echo "Time: $(date -Iseconds). starting directory bruteforce." >> "$mainfolder"/logfile.txt
+    echo "Time: $(date -Iseconds). starting directory bruteforce." >> ./externum-results/logfile.txt
     echo -e "${blue}Busting some directories and looking for codes: ${colouroff}"
-    if ! command -v dirsearch &>/dev/null; then
+    if ! command -v ffuf &>/dev/null; then
         banner
         echo -e ${red}"Badtimes! You need ffuf installed for Externum to work"${colouroff}
         exit
@@ -174,17 +172,17 @@ quickenum() {
             echo -e ${red}"Badtimes! You have forgotten to add the path to your wordlist"${colouroff}
             exit
         else
-            for line in $(cat "$mainfolder"/enum/webservers.txt); do
-               ffuf -u $line/FUZZ -H "User-Agent: Firefox" -w "$wordlist" -t 10 -mc 200,204,401,403,500,501,502 -recursion -recursion-depth 1 -s -of csv -o $mainfolder/enum/directories/$(echo $line | cut -d "/" -f 3).csv > /dev/null 2>&1 &
+            for line in $(cat ./externum-results/enum/webservers.txt); do
+               ffuf -u $line/FUZZ -H "User-Agent: Firefox" -w "$wordlist" -t 10 -mc 200,204,401,403,500,501,502 -recursion -recursion-depth 1 -s -of csv -o ./externum-results/enum/directories/$(echo $line | cut -d "/" -f 3).csv > /dev/null 2>&1 &
                spinner
             done
         fi   
     fi
-    echo "Time: $(date -Iseconds). directory busting complete." >> "$mainfolder"/logfile.txt
+    echo "Time: $(date -Iseconds). directory busting complete." >> ./externum-results/logfile.txt
     
     #nikto
     echo
-    echo "Time: $(date -Iseconds). starting nikto." >> "$mainfolder"/logfile.txt
+    echo "Time: $(date -Iseconds). starting nikto." >> ./externum-results/logfile.txt
     echo -e "${blue}Scanning the webservers using Nikto: ${colouroff}"
     if ! command -v rush &>/dev/null; then
         banner
@@ -196,26 +194,26 @@ quickenum() {
             echo -e ${red}"Badtimes! You need Nikto installed for Externum to work"${colouroff}
             exit
         else #Running nikto multi threaded using rush
-            rush "nikto --ask no --maxtime 15m -host {} -Format html -o "$mainfolder"/enum/nikto/AllResults.html" -i "$mainfolder"/enum/webservers.txt -j 10 &
+            rush "nikto --ask no --maxtime 15m -host {} -Format html -o ./externum-results/enum/nikto/AllResults.html" -i ./externum-results/enum/webservers.txt -j 10 &
             spinner
         fi
-        echo "Time: $(date -Iseconds). nikto complete." >> "$mainfolder"/logfile.txt
+        echo "Time: $(date -Iseconds). nikto complete." >> ./externum-results/logfile.txt
     fi
     
 
     #Screenshots GoWitness
     echo
-    echo "Time: $(date -Iseconds). starting gowitness screen capture." >> "$mainfolder"/logfile.txt
+    echo "Time: $(date -Iseconds). starting gowitness screen capture." >> ./externum-results/logfile.txt
     echo -e "${blue}Grabbing some screenprints, nearly finshed.${colouroff}"
     if ! command -v gowitness &>/dev/null; then
         banner
         echo -e ${red}"Badtimes! You need GoWitness installed for Externum to work"${colouroff}
         exit
     else
-        gowitness file -f "$mainfolder"/enum/webservers.txt -t 10 --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0" -P "$mainfolder"/enum/screenshots/captures --db-path "$mainfolder"/enum/screenshots/captures.sqlite3 > /dev/null 2>&1 &
+        gowitness file -f ./externum-results/enum/webservers.txt -t 10 --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0" -P ./externum-results/enum/screenshots/captures --db-path ./externum-results/enum/screenshots/captures.sqlite3 > /dev/null 2>&1 &
         spinner
     fi
-    echo "Time: $(date -Iseconds). gowitness complete." >> "$mainfolder"/logfile.txt
+    echo "Time: $(date -Iseconds). gowitness complete." >> ./externum-results/logfile.txt
 
 }
 
@@ -238,22 +236,22 @@ nmap() {
     echo -e ${blue}
     echo "Dont forget you will need to be root for this section, you will be prompted for the password next"
     echo
-    echo "Time: $(date -Iseconds). starting nmap TCP all ports." >> "$mainfolder"/logfile.txt
+    echo "Time: $(date -Iseconds). starting nmap TCP all ports." >> ./externum-results/logfile.txt
     echo "Starting NMAP TCP/UDP against target IP's"
     echo -e ${colouroff}
 
-    sudo rush "nmap -Pn -sSV -T4 -p- {} -oN "$mainfolder"/enum/nmap/tcp/{}" -i "$mainfolder"/targets.txt -j 5 > /dev/null 2>&1 &
+    sudo rush "nmap -Pn -sSV -T4 --top-ports 1000 {} -oN ./externum-results/enum/nmap/tcp/{}" -i ./externum-results/targets.txt -j 5 > /dev/null 2>&1 &
     spinner
     
-    echo "Time: $(date -Iseconds). completed nmap TCP all ports." >> "$mainfolder"/logfile.txt
+    echo "Time: $(date -Iseconds). completed nmap TCP all ports." >> ./externum-results/logfile.txt
 
-    echo "Time: $(date -Iseconds). starting nmap UDP top 200 ports." >> "$mainfolder"/logfile.txt
+    echo "Time: $(date -Iseconds). starting nmap UDP top 200 ports." >> ./externum-results/logfile.txt
     echo -e ${blue}"Starting NMAP UDP against target IP's${colouroff}"
     
-    sudo rush "nmap -Pn -sUV --top-ports 200 {} -oN "$mainfolder"/enum/nmap/udp/{}" -i "$mainfolder"/targets.txt -j 5 > /dev/null 2>&1 &
+    sudo rush "nmap -Pn -sUV --top-ports 200 {} -oN ./externum-results/enum/nmap/udp/{}" -i ./externum-results/targets.txt -j 5 > /dev/null 2>&1 &
     spinner
     
-    echo "Time: $(date -Iseconds). completed nmap UDP top 200 ports." >> "$mainfolder"/logfile.txt
+    echo "Time: $(date -Iseconds). completed nmap UDP top 200 ports." >> ./externum-results/logfile.txt
 
 }
 
